4c55f0495df33b472962aa186130c2b17119bdcd
iMac <tanks0@qq.com>
2018-09-10 01:25:28 +0800

    添加 通过key取出json值工具类方法

--- src/main/java/com/xuexibao/fastgun/utils/JsonUtil.java
+++ src/main/java/com/xuexibao/fastgun/utils/JsonUtil.java
@@ -3,6 +3,8 @@ package com.xuexibao.fastgun.utils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.Map;
+
 public class JsonUtil {
 
     /**
@@ -16,4 +18,16 @@ public class JsonUtil {
         Gson gson = new Gson();
         return  gson.toJson(object);
 }
+
+    /**
+     * 取出 json的key值
+     * @param json
+     * @param key
+     * @return
+     */
+    public static String getValue(String json,String key){
+        Gson gson = new Gson();
+        Map map = gson.fromJson(json, Map.class);
+        return (String) map.get(key);
+    }
 }
